085a84e0656022a547f1d11c194508ae501d00f1
DESKTOP-8AR3IAJ\USER PC <busariroqeeb16@gmail.com>
2025-11-01 15:59:05 +0100

    feat: install convex

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import "./App.css";
 import CustomRadio from "./components/form_elements/CustomRadio/CustomRadio";
 import TextField from "./components/form_elements/TextField/TextField";
 import NumberSelector from "./components/form_elements/NumberSelector/NumberSelector";
+import { useQuery } from "convex/react";
+import {api} from "../convex/_generated/api"
 
 function App() {
   const [radioValue, setRadioValue] = useState("");
   const [quantity, setQuantity] = useState(1);
+  const tasks = useQuery(api.tasks.get)
   return (
     <>
+    {tasks ? tasks.map((t)=>t.text):"No Tasks"}
       <TextField
         label="Name"
         onChange={() => {}}
