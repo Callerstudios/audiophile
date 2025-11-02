b52e97cfcb5dd19f41bc6b7aa3f8467b6c9dcd2d
DESKTOP-8AR3IAJ\USER PC <busariroqeeb16@gmail.com>
2025-11-02 09:50:37 +0100

    feat: add variants for button

--- src/components/Button1.tsx
+++ src/components/Button1.tsx
@@ -2,17 +2,24 @@ import type React from "react";
 
 type ButtonProps = {
   content: string;
-  type: "primary" | "secondary";
+  type: "primary" | "secondary" | "black";
   onClick: () => void;
 };
 const Button1: React.FC<ButtonProps> = ({ content, type, onClick }) => {
+  const styles: Record<ButtonProps["type"], string> = {
+    primary: "bg-brown-1 hover:bg-brown-2 text-white",
+    secondary:
+      " bg-transparent border border-black hover:bg-black text-black hover:text-white",
+    black: "bg-black hover:bg-white hover:text-black text-white",
+  };
   return (
     <div
-      className={`w-40 h-12  flex justify-center items-center ${type == "primary" ? "bg-brown-1 hover:bg-brown-2" : "      bg-white border-black hover:bg-black"}`}
+      className={`w-40 h-12 cursor-pointer
+         flex justify-center items-center ${styles[type]}`}
       onClick={onClick}
     >
       <small
-        className={`font-bold w-full h-full flex items-center justify-center ${type === "primary" ? "text-white" : "text-black hover:text-white"}`}
+        className={`font-bold w-full h-full uppercase flex items-center justify-center`}
       >
         {content}
       </small>
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -2,8 +2,10 @@ import headphoneImage from "../assets/product-xx99-mark-one-headphones/desktop/i
 import speakerImage from "../assets/product-zx7-speaker/desktop/image-product.png";
 import earphoneImage from "../assets/product-yx1-earphones/desktop/image-product.png";
 import Product from "./Product";
-import styles from "./ComponentsStyle.module.css"
-import curves from "../assets/backgrounds/curve.png"
+import styles from "./ComponentsStyle.module.css";
+import curves from "../assets/backgrounds/curve.png";
+import speakerImage2 from "../assets/product-zx9-speaker/desktop/image-product.png";
+import Button1 from "./Button1";
 
 const Products = () => {
   return (
@@ -16,7 +18,33 @@ const Products = () => {
       <div
         className={`${styles.speakerBg} w-full h-140 bg-no-repeat bg-cover aspect-video relative`}
       >
-        <img src={curves} alt="Curves" className="absolute inset-0" />
+        <img src={curves} alt="Curves" className="absolute inset-0 z-0" />
+        <div className="flex flex-row gap-20 items-center h-full">
+          <div className="w-fit flex flex-row self-end">
+            <img src={speakerImage2} className="" alt="Speaker Image" />
+          </div>
+          <div
+            className="flex flex-col max-w-87 gap-4 text-white w-3/6  h-fit z-1"
+            style={{ marginTop: "100px" }}
+          >
+            <h1>ZX9 SPEAKER</h1>
+            <p>
+              Experience natural, lifelike audio and exceptional build quality
+              made for the passionate music enthusiast.
+            </p>
+            <Button1
+              content="SEE PRODUCT"
+              type="black"
+              onClick={() => {}}
+            />
+          </div>
+        </div>
+      </div>
+      <div className={`${styles.speakerBg2} h-80 w-full flex flex-row bg-black items-center pl-[10%]`}>
+        <div className="w-fit flex flex-col gap-6"> 
+          <h5>ZX7 SPEAKER</h5>
+          <Button1 content="see product" onClick={() => {}} type="secondary" />
+        </div>
       </div>
     </section>
   );
