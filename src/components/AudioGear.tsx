a10580fcf9f6b130032e12cc561324d7cc5a7fea
DESKTOP-8AR3IAJ\USER PC <busariroqeeb16@gmail.com>
2025-11-08 11:37:01 +0100

    feat: fix image width

--- src/components/AudioGear.tsx
+++ src/components/AudioGear.tsx
@@ -1,16 +1,13 @@
 import manImage from "../assets/man.png";
-import { useScreenSize } from "../hooks/useScreenSize";
+
 const AudioGear = () => {
-  const screen = useScreenSize();
   return (
-    <div className="flex flex-col max-w-full gap-5 md:max-w-full lg:flex-row px-[10%] justify-between items-center my-30">
-      <div
-        className={`md:max-w-102 flex flex-col gap-5 ${screen === "mobile" ? "order-2" : ""}`}
-      >
-        <h2 style={{ fontSize: screen === "mobile" ? "28px" : "" }}>
+    <div className="flex flex-col-reverse lg:flex-row items-center justify-between gap-5 px-[10%] my-30 max-w-full">
+      <div className="w-full lg:w-1/2 flex flex-col gap-5 text-center lg:text-left">
+        <h2 className="text-3xl md:text-4xl font-bold">
           Bringing you the <span className="text-brown-1">best</span> audio gear
         </h2>
-        <p>
+        <p className="text-gray-600">
           Located at the heart of New York City, Audiophile is the premier store
           for high end headphones, earphones, speakers, and audio accessories.
           We have a large showroom and luxury demonstration rooms available for
@@ -19,7 +16,12 @@ const AudioGear = () => {
           best place to buy your portable audio equipment.
         </p>
       </div>
-      <img src={manImage} alt="Man Image" />
+
+      <img
+        src={manImage}
+        alt="Man Image"
+        className="w-full lg:w-1/2 rounded-xl"
+      />
     </div>
   );
 };
